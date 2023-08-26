b13befe9be8cda7085829e9d298690cb2d337530
JoyBoyCr7 <Rnyongkah@gmail.com>
2023-08-26 08:46:41 -0500

    nav

--- src/components/header.js
+++ src/components/header.js
@@ -3,20 +3,23 @@ import { Link } from 'react-router-dom'
 const Header = (props) => {
     return <nav className='header'>
         <div className='logo-container'> 
-            <Link to='/'>
+            <Link to='/' className='headerlink'>
             <h1>APEFOOD</h1>
             </Link>
         </div>
-            <div className='signup'>
-             <Link to='user/login'> 
-             User login
-             </Link>
+            <div className='together'>
+                <div className='signup' >
+                <Link to='user/login' className='headerlink'> 
+                User login
+                </Link>
+                </div>
+                <div className='signin'>
+                <Link to='farmer/login' className='headerlink'> 
+                Farmer login
+                </Link>
+                </div>
             </div>
-            <div className='signin'>
-            <Link to='farmer/login'> 
-            Farmer login
-            </Link>
-            </div>
+        
             
     </nav>
 }
