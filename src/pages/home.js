25f6f537e586cbdc59538c400f856a981c5d8b22
abanobmorkos1 <abanobmorkos11@gmail.com>
2023-08-26 09:53:49 -0400

    more csss

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,40 +3,42 @@ import { farmsAndProductsLoader } from '../loaders';
 
 
 const Home = () => {
-    const { farms, products }  = useLoaderData(farmsAndProductsLoader())
+    const { farms, products } = useLoaderData(farmsAndProductsLoader());
 
+    console.log(farms);
 
-
-    console.log(farms)
     return (
-        <div className='proname'>
-        <h2>Products</h2>
-        <div className='home-container'>
-            {products.map((product, index) => (
-        
-                <div key={product._id} className='card'>
-                    <Link to={`product/${product._id}`}>
-                        <h1>{product.productname}</h1>
-                    </Link>
-                    <img src={product.image} alt={product.name} />
-                    <h2>${product.price}</h2>
-                    <h3>Description: {product.description}</h3>
-                </div>
-            ))}
-        </div>
+        <div className=''>
+            <h1>Products</h1>
+            <div className='grid-container'>
             
-        {farms.map((farm, index) => (
-                <div key={farm._id} className='farmname'>
-                    <Link to={`farm/${farm._id}`}>
-                        <h1>{farm.farmname}</h1>
-                    </Link>
-                    <img src={farm.image} alt={farm.name} />
-                    <h2>{farm.address}</h2>
-                    <h2>{farm.city} ,{farm.state}</h2>
-                    
-                </div>
-            ))}
+                {products.map((product, index) => (
+                    <div key={product._id} className='card'>
+                        <Link to={`product/${product._id}`}>
+                            <h1>{product.productname}</h1>
+                        </Link>
+                        <img src={product.image} alt={product.name} />
+                        <h2> Price: <span> ${product.price}</span></h2>
+                        <h3>Description: <span> {product.description}</span> </h3>
+                    </div>
+                ))}
+            </div>
+            
+            <h2>Farms</h2>
+            <div className='grid-container'>
+                {farms.map((farm, index) => (
+                    <div key={farm._id} className='card'>
+                        <Link to={`farm/${farm._id}`}>
+                            <h1>{farm.farmname}</h1>
+                        </Link>
+                        <img src={farm.image} alt={farm.name} />
+                        <h2>{farm.address}</h2>
+                        <h2>{farm.city}, {farm.state}</h2>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
+
 export default Home;
